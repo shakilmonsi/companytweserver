36966749a88c06347528a01572d5e0b3582829c5
shakilmia420 <samdshakilofficial@gmail.com>
2023-12-23 18:59:47 +0600

    cerifyjs

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+// jwt  verifyjwt 
+
+
+const  verifyjwt = (req,res,next)=>{
+  const authorization =req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true , message: "unauthorized eccess"})
+  }
+
+  // beareer token 
+
+  const token = authorization.split(" ")[1];
+  jwt.verify(token,process.env.ACCCSS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res.status(401).send({error: true ,message: "unauthorized access"})
+    }
+    req.decoded= decoded;
+    next();
+  }  )
+}
 
 
 
